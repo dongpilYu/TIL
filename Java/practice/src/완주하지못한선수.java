2ea3d699cb0141c2b7de4e3a35afecea1e33285f
dongpil_yu <dongpil_yu@tmax.co.kr>
2022-01-15 17:48:04 +0900

    완주하지못한선수 다시 풀어보기

--- Java/practice/src/완주하지못한선수.java
+++ Java/practice/src/완주하지못한선수.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class 완주하지못한선수 {
     public static void main(String[] args){
@@ -11,12 +10,14 @@ public class 완주하지못한선수 {
 
         String result = 완주하지못한선수.solution(participant, completion);
         System.out.println(result);
+
     }
     static String solution(String[] participant, String[] completion){
         String answer = "";
         HashMap<String, Integer> hm = new HashMap<>();
         for(String player : participant)
             hm.put(player, hm.getOrDefault(player, 0) + 1);
+
         for(String player : completion){
             if(hm.get(player) == 1)
                 hm.remove(player);
