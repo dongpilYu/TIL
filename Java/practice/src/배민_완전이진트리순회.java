c96c67a0f5214785568c6a8b4bf6a1cb7500eff5
dongpil_yu <dongpil_yu@tmax.co.kr>
2022-01-15 21:39:54 +0900

    배민-완전이진트리순회

---
+++ Java/practice/src/배민_완전이진트리순회.java
@@ -0,0 +1,15 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 배민_완전이진트리순회 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ");
+        sb.append(br.readLine());
+
+    }
+}
